0f61746807b26d5c8481e2af265e4f3a12867027
Adrian Lynch <adrian.lynch@concrete.cc>
2014-11-10 22:23:31 +0000

    Markdown preview + CSS

--- jstips.js
+++ jstips.js
@@ -1,16 +1,9 @@
 Tips = new Mongo.Collection("tips");
 
 if (Meteor.isClient) {
-	// counter starts at 0
-	//Session.setDefault("counter", 0);
 
 	Accounts.ui.config({
 		passwordSignupFields: 'USERNAME_AND_EMAIL', // USERNAME_AND_EMAIL USERNAME_AND_OPTIONAL_EMAIL USERNAME_ONLY EMAIL_ONLY
-		// extraSignupFields: [{
-		// 	fieldName: 'displayName',
-		// 	fieldLabel: 'Display name'
-		// }]
-		// Look at: http://stackoverflow.com/questions/23507384/adding-more-fields-to-meteor-user-accounts
 	});
 
 	Template.tips.helpers({
@@ -19,23 +12,33 @@ if (Meteor.isClient) {
 		}
 	});
 
-	/*Template.hello.events({
-		'click button': function () {
-			// increment the counter when button is clicked
-			Session.set("counter", Session.get("counter") + 1);
+	Template.tips.events({
+		"click button": function () {
+			Tips.remove(this._id);
 		}
-	});*/
+	});
+
+	Template.tipForm.helpers({
+		tipPreview: function() {
+			// How to populate this object?
+			return {
+				title: "Title...",
+				created: new Date(),
+				owner: {
+					username: "Username"
+				},
+				body: "__bold__"
+			};
+		}
+	});
 
 	Template.tipForm.events({
 		"submit form": function(e) {
-			var title = e.target.title.value;
-			var body = e.target.body.value;
-			console.log("Adding: ", title, body);
-			console.log(Meteor.user());
-			// CONTUINUE: Add logged in user details to the tip - Find a way to require username AND password
+			var title = e.target.title;
+			var body = e.target.body;
 			Tips.insert({
-				title: title,
-				body: body,
+				title: title.value,
+				body: body.value,
 				created: new Date(),
 				approved: true, // Whilst developing - Will be an admin feature
 				owner: {
@@ -43,14 +46,26 @@ if (Meteor.isClient) {
 					username: Meteor.user().username
 				}
 			});
+			title.value = "";
+			body.value = "";
+			document.getElementById("preview").innerHTML = "";
 			return false;
+		},
+		"click input[name='myTips']": function(e) {
+			Session.set("title", e.target.value);
+		},
+		"keyup textarea[name='body']": function(e) {
+			var body = e.target.value;
+			var preview = "";
+			try {
+				preview = marked(body, {sanitize: true});
+			} catch(e) {
+				// Do nothing - This is crap but it'll work for now
+			}
+			document.getElementById("preview").innerHTML = preview;
 		}
 	});
 
-}
+	marked('# Marked in browser\n\nRendered by **marked**.');
 
-if (Meteor.isServer) {
-	Meteor.startup(function () {
-		//
-	});
 }
